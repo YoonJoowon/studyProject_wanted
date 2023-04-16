a93c82ed125232e7e74478e53209f42d918d5050
yoonjoowon <wndnjs6224@naver.com>
2023-04-16 20:41:21 +0900

    0416

--- wanted/main.js
+++ wanted/main.js
@@ -52,34 +52,81 @@ modal2_close.addEventListener("click", function() {
 
 
 // slide1
-const prevButton = document.querySelector('.subSlideBtn1');
-const nextButton = document.querySelector('.subSlideBtn2');
-const carousel = document.querySelector('.carousel');
-let index = 0;
+let slides = document.querySelector('.slides');
+let slideImg = document.querySelectorAll('.slides li');
+currentIdx = 0;
+slideCount = slideImg.length;
+prev = document.querySelector('.prev'); //이전 버튼
+next = document.querySelector('.next'); //다음 버튼
 
-prevButton.addEventListener('click', () => {
-   if (index === 0){
-    index = 6
-   }
-   index -= 1;
-   
-   carousel.style.transform = `translate3d(-${1084 * index}px, 0, 0)`;
+slideWidth = 1084; //슬라이드이미지 넓이
+slideMargin = 0; //슬라이드 끼리의 마진값
+
+makeClone(); // 처음이미지와 마지막 이미지 복사 함수
+initfunction(); //슬라이드 넓이와 위치값 초기화 함수
+
+function makeClone() {
+  let cloneSlide_first = slideImg[0].cloneNode(true);
+  let cloneSlide_last = slides.lastElementChild.cloneNode(true);
+  slides.append(cloneSlide_first);
+  slides.insertBefore(cloneSlide_last, slides.firstElementChild);
+}
+
+function initfunction() {
+  slides.style.width = (slideWidth + slideMargin) * (slideCount + 2) + 'px';
+  slides.style.left = -(slideWidth + slideMargin) + 'px';
+}
+
+
+next.addEventListener('click', function () {
+  //다음 버튼 눌렀을때
+  if (currentIdx <= slideCount - 1) {
+    //슬라이드이동
+    slides.style.left = -(currentIdx + 2) * (slideWidth + slideMargin) + 'px';
+    slides.style.transition = `${0.5}s ease-out`; //이동 속도
+  }
+  if (currentIdx === slideCount - 1) {
+    //마지막 슬라이드 일때
+    setTimeout(function () {
+      //0.5초동안 복사한 첫번째 이미지에서, 진짜 첫번째 위치로 이동
+      slides.style.left = -(slideWidth + slideMargin) + 'px';
+      slides.style.transition = `${0}s ease-out`;
+    }, 500);
+    currentIdx = -1;
+  }
+  currentIdx += 1;
 });
 
-nextButton.addEventListener('click', () => {
-   if (index === 5){
-    index = -1;
-   }
-   index += 1;
-   
-   carousel.style.transform = `translate3d(-${1084 * index}px, 0, 0)`;
+prev.addEventListener('click', function () {
+  //이전 버튼 눌렀을때
+  console.log(currentIdx);
+  if (currentIdx >= 0) {
+    slides.style.left = -currentIdx * (slideWidth + slideMargin) + 'px';
+    slides.style.transition = `${0.5}s ease-out`;
+  }
+  if (currentIdx === 0) {
+    setTimeout(function () {
+      slides.style.left = -slideCount * (slideWidth + slideMargin) + 'px';
+      slides.style.transition = `${0}s ease-out`;
+    }, 500);
+    currentIdx = slideCount;
+  }
+  currentIdx -= 1;
 });
 
-setInterval(() => {
-    if (index === 5) {
-        index = -1;
-    }
-    index += 1;
-    carousel.style.transform = `translate3d(-${1084 * index}px, 0, 0)`;
-  }, 3000);
 
+function slideNext() {
+  next.click();
+}
+
+let timer = setInterval(slideNext, 3000);
+
+// 마우스가 슬라이드 위에 있을 때 타이머를 멈추도록 이벤트 핸들러 추가
+slides.addEventListener('mouseenter', function() {
+  clearInterval(timer);
+});
+
+// 마우스가 슬라이드 밖으로 이동할 때 타이머를 다시 시작하도록 이벤트 핸들러 추가
+slides.addEventListener('mouseleave', function() {
+  timer = setInterval(slideNext, 3000);
+});
